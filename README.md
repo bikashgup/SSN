1a7f52b1b461317a608d64eaceb7aefc6eb66dbb
bikashgup <bikash.gupta@fusemachines.com>
2023-03-22 10:27:36 +0545

    first Integration of autho

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SSN
 Simple Sample Social Network
+
+#project initialization
+npm init -y
+
+#Installing reuirements for the project
+npm install express
+npm install nodemon --save-dev
+npm install oidc-provider ejs
+
---
+++ app.js
@@ -0,0 +1,49 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const path = require('path');
+const router = require('./routes/index');
+const { auth} = require('express-openid-connect');
+
+dotenv.config();
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+
+const port = process.env.PORT || 3000;
+
+app.use(auth(config));
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
+
+app.use('/', router);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+app.listen(port, () => {
+    console.log(`SSN Listening on ${config.baseURL}`);
+  });
